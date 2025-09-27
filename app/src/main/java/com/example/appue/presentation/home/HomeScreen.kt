06ead58bb4004d8b07ff3cec19e5ee64c6081425
Jummy-gr <22200224@ue.edu.pe>
2025-09-27 10:54:03 -0500

    Nuevo cambios en HomeScreen.kt

--- app/src/main/java/com/example/appue/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/appue/presentation/home/HomeScreen.kt
@@ -1,13 +1,17 @@
 package com.example.appue.presentation.home
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,20 +21,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.example.appue.data.model.CountryModel
 
 
 
 
 val mockedCountries = listOf(
-    CountryModel("Colombia", 8, "https://flagcdn.com/w320/br.png"),
-    CountryModel("Argentina", 1, "https://flagcdn.com/w320/br.png"),
-    CountryModel("Francia", 4, "https://flagcdn.com/w320/br.png"),
+    CountryModel("Colombia", 8, "https://flagcdn.com/w320/co.png"),
+    CountryModel("Argentina", 1, "https://flagcdn.com/w320/ar.png"),
+    CountryModel("Francia", 4, "https://flagcdn.com/w320/fr.png"),
     CountryModel("Brasil", 3, "https://flagcdn.com/w320/br.png"),
-    CountryModel("Perú", 62, "https://flagcdn.com/w320/br.png"),
-    CountryModel("Uruguay", 9, "https://flagcdn.com/w320/br.png")
+    CountryModel("Perú", 62, "https://flagcdn.com/w320/pe.png"),
+    CountryModel("Uruguay", 9, "https://flagcdn.com/w320/uy.png")
 )
 
 
@@ -53,7 +59,29 @@ fun HomeScreen()
 
         LazyColumn {
             items(mockedCountries){ country ->
-                Text("Country: ${country.name}")
+                //Text("Country: ${country.name}")   SE SACO DEL DISEÑO PORQUE SE AGREGÓ UNA CARD
+
+                Card(
+                    modifier = Modifier.fillMaxSize().padding(vertical =8.dp)
+                ){
+
+                    Row(modifier = Modifier.padding(12.dp)) {  //agregado para el margen del diseño
+
+                        Image(
+                            contentDescription = country.name,
+                            modifier = Modifier.size(64.dp),
+                            contentScale = ContentScale.Crop,
+                            painter = rememberAsyncImagePainter(country.imagenUrl)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Column {
+                            Text(text = country.name, style = MaterialTheme.typography.titleMedium)
+                            Text(text = "Ranking FIFA: ${country.ranking}")
+                        }
+                    }
+
+                }
+
             }
 
         }
